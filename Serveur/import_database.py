121f321f4aa10e3dba64f0b0dc7d6a96a06bc17f
CharletRph <raphael.charlet@ecl19.ec-lyon>
2020-06-12 11:26:51 +0200

    Fonction d'importation de data v1
    ajouter les clefs au fur et à mesure de l'ajout dans la base de donnnées

---
+++ Serveur/import_database.py
@@ -0,0 +1,40 @@
+
+#import sqlite3
+#
+#table=sqlite3.connect('pays.sqlite')
+
+def importation ():
+    import sqlite3
+    table=sqlite3.connect('pays.sqlite')
+    datalist=[]
+    #Liste à mettre à jour en fonction de l'avancée dans le projet
+    keys=['nom_conventionnel','nom_commum','capitale','continent','drapeau','latitude','longitude']
+    
+    #Récupération d'un tuple des noms conventionnels
+    c=conn.cursor()
+    requete='SELECT nom_conventionnel FROM global'
+    c.execute(requete)
+    r=c.fetchone()
+    
+    identification=1
+    for country in r:
+        donnees=read_pays(table,country)
+        #Initialisation du dico
+        datalist.append({'id':identification})
+        identification+=1
+        
+        for i in range(len(keys)):
+            datalist[-1][keys[i]]=donnees[i]
+    
+    return datalist
+    
+        
+
+def read_pays(conn,pays):
+    c=conn.cursor()
+    requete='SELECT * FROM global WHERE nom_conventionnel=?'
+    c.execute(requete,(pays,))
+    r=c.fetchone()
+    return r
+
+
