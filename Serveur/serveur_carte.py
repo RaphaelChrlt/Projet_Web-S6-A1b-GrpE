6f59415ff43bcb68fa64282333884341b494c981
Lacaze Max <max.lacaze@ecl19.ec-lyon.fr>
2020-06-12 10:31:23 +0200

    Texte justifié, deux marqueurs

--- Serveur/serveur_carte.py
+++ Serveur/serveur_carte.py
@@ -32,12 +32,13 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
 
     # requete location - retourne la liste de lieux et leurs coordonnées géographiques
     if self.path_info[0] == "location":
-      data=[{'id':1,'lat':0,'lon':0,'name':"Le pays test"}]
+      data=[{'id':1,'lat':0,'lon':0,'name':"Le pays test"},{'id':2,'lat':10,'lon':10,'name':"Le pays test deuuuuux le retour"}]
       self.send_json(data)
 
     # requete description - retourne la description du lieu dont on passe l'id en paramètre dans l'URL
     elif self.path_info[0] == "description":
-      data=[{'id':1,'desc':"Ceci est un test pour voir où s'affiche la description, et c'est un texte long pour vrifier la mise en page "}]
+      data=[{'id':1,'desc':"Ceci est un test pour voir où s'affiche la description, et c'est un texte long pour vérifier la mise en page: Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, , comes from a line in section 1.10.32. "},
+             {'id':2,'desc':"blalal blakfsdk,f pfqd,fv^k ,sf,vkf,d sdlfkv,dfkv,"}]
       for c in data:
         if c['id'] == int(self.path_info[1]):
           self.send_json(c)
